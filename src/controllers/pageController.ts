15f7289e248ceeb0d5f475b9a5f9e7fd9474ec03
Aluno IFSP <aluno@car.ifsp>
2026-03-16 08:36:22 -0300

    Ajustando views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,22 +2,37 @@ import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
     // res.send('home');
-    res.render("pages/page"), {
+    res.render("pages/page", {
         banner: {
             title: 'Todos os animais',
             backgrond: 'allanimals.jpg'
         }
-    }
+    });
 };
 
 export const dogs = (req: Request, res: Response) => {
-    res.render("pages/page");
+    res.render("pages/page", {
+        banner: {
+            title: 'Cachorros',
+            backgrond: 'banner_dog.jpg'
+        }
+    });
 };
 
 export const cats = (req: Request, res: Response) => {
-    res.render("pages/page");
+    res.render("pages/page", {
+        banner: {
+            title: 'Gatos',
+            backgrond: 'banner_cat.jpg'
+        }
+    });
 };
 
 export const fishes = (req: Request, res: Response) => {
-    res.render("pages/page");
+    res.render("pages/page", {
+        banner: {
+            title: 'Peixes',
+            backgrond: 'banner_fish.jpg'
+        }
+    });
 };
