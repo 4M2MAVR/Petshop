9bd1d9eb754f00b4e1a208383995ac8ea63fede7
MATHEUS DE ALMEIDA VILLEGAS REIS <CG3037819@car.ifsp>
2026-03-18 08:25:28 -0300

    Ajustando menu

---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,15 @@
+type MenuOptions = ' ' | 'all' | 'dogs' | 'cats' | 'fishes'
+
+export const createMenuObject = (activeMenu: MenuOptions) => {
+    let returnObject = {
+        all: false,
+        dogs: true,
+        cats: false,
+        fishes: false
+    }
+    if(activeMenu !== ' ') {
+        returnObject[activeMenu] = true;
+    }
+
+    return returnObject
+}
